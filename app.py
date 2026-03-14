49d0c5ba06601d90093c56be58227e00881586a6
rodalcham <rodalcham@gmail.com>
2026-03-14 17:08:03 +0100

    Bug fixes

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ h1, h2, h3 { font-family: 'Playfair Display', serif; }
 
 .title {
     font-family: 'Playfair Display', serif;
-    font-size: 4rem;
+    font-size: 10.0rem;
     color: #e8d5a3;
     letter-spacing: 0.02em;
     margin-bottom: 0;
