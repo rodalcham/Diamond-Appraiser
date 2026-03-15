7dfbaf275e22d2287d84e30b290687de87c35864
rodalcham <rodalcham@gmail.com>
2026-03-15 14:09:33 +0100

    Adding Log linear regrssion for fun

--- app.py
+++ app.py
@@ -236,7 +236,7 @@ for ax, y_pred_plot, title, r2_val, rmse_val, pred_val in zip(
     ax.scatter(y_test[idx], y_pred_plot[idx], alpha=0.3, s=8, color='#4a90d9')
 
     # Perfect prediction line
-    max_val = min(y_test.max(), y_pred.max(), exp_y_pred_log.max()) # We use min instead of max because Outliers don't allow to visualize the graph otherwise
+    max_val = max(y_test.max(), y_pred.max(), exp_y_pred_log.max()) # We use min instead of max because Outliers don't allow to visualize the graph otherwise
     ax.plot([0, max_val], [0, max_val], color='#c9a84c', linewidth=1.5, linestyle='--', label='Perfect fit')
     
     # Mark the predicted price for the entered diamond
